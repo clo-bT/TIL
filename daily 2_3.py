9464046073d00a06855473299d551f3ac63b7973
clo-bT <tael8213@gmail.com>
2023-01-17 17:46:07 +0900

    0117실습

---
+++ daily 2_3.py
@@ -0,0 +1,20 @@
+str_lst = list(input('문자열을 입력하세요. : ').lower().split())
+# 
+# if str_lst[i][-1] == str_lst[i+1][0]:
+a = 0
+print(str_lst)
+b = int(len(str_lst))
+for i in range (b):
+    if (str_lst[i][-1] == str_lst[i+1][0]):
+        a += 1
+        if a == b - 1:
+            print('Pass')
+            break
+    else:
+        print('Fail')
+        break
+
+
+
+    
+
