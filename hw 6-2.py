a6f790383a22bdc50655b8e37aa82fe8c83c3931
clo-bT <tael8213@gmail.com>
2023-01-20 09:18:40 +0900

    0119숙제 소금물

--- hw 6-2.py
+++ hw 6-2.py
@@ -7,7 +7,7 @@
 # 출력 예시
 # 4.0% 700.0g
 
-'''
+'''내가 해볼게
 salt = []
 mul = []
 i, n = 1 ,0
@@ -35,25 +35,55 @@ while True:
 print(f'{nongdo}% {mul_all}g')
 '''
 
-temp1 = []
-temp2 = []
-temp3 = []
+nongdo = []
+saltwater = []
+s = []
+c = 0
+# input 받기
+while True:
+    salts = list(map(str,input('소금물의 농도(%)와 소금물의 양(g)을 입력하십시오: ').split()))
+    if salts[0] == 'Done':
+        break
+    
+    else:
+        salt = int(salts[1]) * (int(salts[0]) / 100)
+        nongdo.append(int(salts[0]))
+        saltwater.append(int(salts[1]))
+        s.append(salt)
+    c += 1
+    if c == 5:
+        break
+
+salt_all = sum(s)
+saltwater_all = sum(saltwater)
+salt_con_all = salt_all / saltwater_all * 100
+print(f'{round(salt_con_all, 2)}%, {saltwater_all}g ')
+
+
+'''
+# 보석님 코드 변수명만 바꾼 거 
+
+nongdo = []
+saltwater = []
+s = []
 cnt = 0
 # 소금물의 퍼센트 농도와 양 받기
-while True:
-    salt = list(map(str,list(input('소금물의 농도(%)와 소금물의 양(g)을 입력하십시오: ').split())))
-    if (salt[0] == 'Done') :
+while (True) :
+    salts = list(map(str,list(input('소금물의 퍼센트 농도와 소금물의 양을 입력하세요.: ').split())))
+    if (salts[0] == 'Done') :
         break
     else :
-        salt = int(salt[1].replace('g', '')) * int(salt[0].replace('%', '')) / 100 #소금의 양 계산
-        temp1.append(int(salt[0].replace('%', ''))) # 농도
-        temp2.append(int(salt[1].replace('g', ''))) # 소금물의 양
-        temp3.append(salt)
+        salt = int(salts[1].replace('g',''))*int(salts[0].replace('%',''))/100 #소금의 양 계산
+        nongdo.append(int(salts[0].replace('%',''))) # 농도
+        saltwater.append(int(salts[1].replace('g',''))) # 소금물의 양
+        s.append(salt)
     cnt += 1
-    if (cnt >= 5):
+    if (cnt >=5) :
         break
 # 전체 합의 소금물의 농도와 양 구하기
-salt_all = sum(temp3)
-saltwater_all = sum(temp2)
-salt_con_all = salt_all / saltwater_all * 100
-print('소금물의 양 : {}g 소금물의 농도 : {}%'.format(saltwater_all, round(salt_con_all, 2))) #결과 출력
+salt_all = sum(s)
+saltwater_all = sum(saltwater)
+nongdo_all = round(salt_all / saltwater_all * 100, 2)
+print(f'{nongdo_all}%, {saltwater_all}g') #결과 출력
+
+'''
