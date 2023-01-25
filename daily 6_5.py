ae503fc92cc8872419cbbd6af1851fd60910522c
clo-bT <tael8213@gmail.com>
2023-01-25 16:33:25 +0900

    0125 실습

--- daily 6_5.py
+++ daily 6_5.py
@@ -1,1 +1,10 @@
-sum_of_digit(3904) # 16
+#재귀함수
+def sum_of_digit(a):
+    #만약에 a를 몫이 0이면 a
+    if a // 10 == 0:
+        return a
+    
+    return sum_of_digit(a // 10) + (a % 10)
+
+
+print(sum_of_digit(3904)) # 16
