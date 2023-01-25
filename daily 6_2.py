b43f5cabccb52422852c899d5f42d1327c0ee841
clo-bT <tael8213@gmail.com>
2023-01-25 15:18:25 +0900

    0125 실습

--- daily 6_2.py
+++ daily 6_2.py
@@ -1,1 +1,21 @@
 grain_lst = [('고구마',3000), ('감자',2000), ('옥수수',4500),('토란',1300)]
+
+# print(type(grain_lst[0][1]))은 int
+def high_price():
+    li = []
+    for i in range(len(grain_lst)):
+        # 가격들 모아서 리스트로 묶어
+        li.append(grain_lst[i][1])
+    # 최대값 구하고
+    m = max(li)
+    # 얘가 누구 가격이었는지 찾기
+    ind = 0
+    for i in range(len(grain_lst)):
+        if grain_lst[i][1] != m:
+            ind += 1
+        else:
+            ind -= 1
+    print(grain_lst[ind][0])
+    
+    
+high_price()
--- daily 6_3.py
+++ daily 6_3.py
@@ -1,2 +1,6 @@
+def count_vowels(a):
+    print(a.count('a')+a.count('e') + a.count('i') + a.count('o') + a.count('u'))
+    return
+
 count_vowels('apple') #=> 2
 count_vowels('banana') #=> 3
