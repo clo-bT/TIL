4b0b76dc926a528726b847e3797e55bd4b3fd906
clo-bT <tael8213@gmail.com>
2023-01-26 09:56:36 +0900

    0126수업

---
+++ 0126수업.py
@@ -0,0 +1,26 @@
+sample_list = [11,22,33,44,55]
+
+# 주어진 리스트의 3번 index에 있는 항목을 제거하고 변수에 할당해 주세요
+print("original list: ",sample_list)
+elem = sample_list.pop(3)
+
+print('list after: ',sample_list)
+print('elem: ',elem)
+
+# sample_list 의 가장 뒤에 77을 추가해보세요
+sample_list.append(77)
+
+# 할당해 놓은 변수의 값을 sample_list의 2번 index에 추가해보세요
+
+print(sample_list)
+sample_list.insert(2,elem)
+print(sample_list)
+
+
+
+
+
+my_tuple = (11, 22, 33, 44, 55, 66)
+
+#주어진 튜플에서 44와 55의 값을 새로운 튜플에 할당해보세요.
+new_tuple = 
