3aba8c5c423665e147900f5ee818ca324b11eb2d
clo-bT <tael8213@gmail.com>
2023-02-03 17:20:12 +0900

    0203 수업

---
+++ SWEA_버스노선.py
@@ -0,0 +1,27 @@
+T = int(input()) #1
+for tc in range(1,T+1):
+    N = int(input()) #2
+    for i in range(N):
+        a, b = map(int,input().split())
+    P = int(input())
+    for j in range(P):
+        pass
+'''
+온라인강의 풀이
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+
+    # 1. N번 반복하면서 노선입력, 빈도수 표시
+    cnts = [0] * 5001
+    for _ in range(N):
+        S, E = map(int, input().split())
+        for i in range(S, E+1):
+            cnts[i] += 1
+    P = int(input())
+    alst = []
+    for _ in range(P):
+        p = int(input())
+        alst.append((cnts[p]))
+    print(f'#{tc}', *alst)
+'''
