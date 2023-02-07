17ba4c578c3b0e657dc55fb0b4fcde58c6ae54d0
clo-bT <tael8213@gmail.com>
2023-02-08 00:38:46 +0900

    0207home

--- studygroup/baek_재귀의 귀재_25501.py
+++ studygroup/baek_재귀의 귀재_25501.py
@@ -12,19 +12,22 @@ print('ABBA:', isPalindrome('ABBA'))
 print('ABC:', isPalindrome('ABC'))
 
 '''
+
 T= int(input()) #5
+
 def recursion(s, l, r):
-    recursion.counter += 1
+    global cnt
+    cnt += 1
     if l>=r:
         return 1
     elif s[l] != s[r]:
         return 0
     else:
         return recursion(s, l+1, r-1)
-
-
+    
 def isPalindrome(s):
     return recursion(s, 0, len(s)-1)
 
 for i in range(T):
-    print(isPalindrome(input()), recursion.counter)
+    cnt = 0
+    print(isPalindrome(input()), cnt)
