045dc53c47f7ec512645155f69209130a857367d
clo-bT <tael8213@gmail.com>
2023-02-16 17:54:46 +0900

    0216

--- algorithm_SWEA/SWEA_6일차계산기1.py
+++ algorithm_SWEA/SWEA_6일차계산기1.py
@@ -28,43 +28,59 @@ for tc in range(1, 11):
 '''
 2+3*4/5
 '''
-for tc in range(1,11):
-    N = int(input())
-    fn = list(input())
-    stack = [0] * N
+op = {'+':1, '-':1, '*':2, '/':2}
+ 
+def postfix(li):
+    stack = [0]*len(li)
     top = -1
-    postfix = ''
+    pfix = ''
  
-    for t in fn:
- 
-        if '0' <= t <= '9':     # 피연산자인 경우
-            postfix += t
-        elif t == '+':               # 연산자인 경우
-            if top == -1:     # 스택이 비어있거나 토큰의 우선순위가 높으면
-                top += 1    # push
+    for t in li:
+        if '0' <= t <= '9':
+            pfix += t
+        elif t in op:
+            if top == -1 or op[stack[top]] < op[t]:
+                top += 1
                 stack[top] = t
             else:
-                while top > -1:         # 스택이 남아있고 토큰의 우선순위가 높을 때 까지
+                while top > -1 and op[stack[top]] >= op[t]:
                     top -= 1
-                    postfix += stack[top+1]
-                top += 1  # push
+                    pfix += stack[top + 1]
+                top += 1
                 stack[top] = t
- 
     while top > -1:
         top -= 1
-        postfix += stack[top + 1]
+        pfix += stack[top+1]
  
+    return pfix
+ 
+def postfix_eval(text):
     stack = []
-    for i in list(postfix):
-        if i.isdigit():  # 숫자
-            stack.append(int(i))
-        else:  # 연산자
-            if len(stack) > 1:  # 연산 가능
-                b = stack.pop()
+    o_li = list(map(str, text))
+    for i in range(len(o_li)):
+        if o_li[i].isdigit():
+            stack.append(int(o_li[i]))
+        else:
+            if len(stack) > 1 and type(stack[-1]) == int:
                 a = stack.pop()
-                if i == '*':
-                    stack.append(a * b)
+                b = stack.pop()
+                if o_li[i] == '+':
+                    stack.append(b+a)
+                elif o_li[i] == '-':
+                    stack.append(b-a)
+                elif o_li[i] == '*':
+                    stack.append(b*a)
                 else:
-                    stack.append(a + b)
-    result = stack.pop()
-    print(f'#{tc}', result)
+                    stack.append(b//a)
+ 
+    return stack
+ 
+ 
+for tc in range(1, 11):
+    N = int(input())
+    op_li = list(map(str, input()))
+    stack = []
+ 
+    post = postfix(op_li)
+    result = postfix_eval(post)
+    print(f'#{tc} {result[0]}')
