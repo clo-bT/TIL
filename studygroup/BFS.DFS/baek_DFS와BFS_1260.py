6045fa8a70bc702f468a3b991b13b91bbf1f12c2
clo-bT <tael8213@gmail.com>
2023-03-08 17:59:58 +0900

    0308study준비

--- studygroup/BFS.DFS/baek_DFS와BFS_1260.py
+++ studygroup/BFS.DFS/baek_DFS와BFS_1260.py
@@ -1,11 +1,12 @@
 from collections import deque
 import sys
-read = sys.stdin.readline
+input = sys.stdin.readline
 
 def bfs(v):
   q = deque()
   q.append(v)       
   visited[v] = 1   
+  
   while q:
     v = q.popleft()
     print(v, end = " ")
@@ -15,11 +16,11 @@ def bfs(v):
         visited[i] = 1
 
 def dfs(v):
-  visited1[v] = 1        
+  visited1[v] = 1    #방문    
   print(v, end = " ")
   for i in range(1, N + 1):
-    if visited1[i] == 0 and graph[v][i] == 1:
-      dfs(i)
+    if visited1[i] == 0 and graph[v][i] == 1:   #방문 안했고 이어져있으면
+        dfs(i)
 
 N, M, V = map(int,input().split()) #4 5 1
 graph = [[0] * (N + 1) for _ in range(N + 1)] 
@@ -29,10 +30,17 @@ visited1 = [0] * (N + 1)
 for _ in range(M):
     a, b = map(int,input().split())
     graph[a][b] = graph[b][a] = 1
+# print(graph) 
+#[[0, 0, 0, 0, 0], 
+# [0, 0, 1, 1, 1], 
+# [0, 1, 0, 0, 1], 
+# [0, 1, 0, 0, 1], 
+# [0, 1, 1, 1, 0]]
 
 dfs(V)
 print()
 bfs(V)
+
 '''
 1 2
 1 3
---
+++ studygroup/BFS.DFS/baek_알고리즘수업-깊이우선탐색1_24479.py
@@ -0,0 +1,6 @@
+import sys
+input = sys.stdin.readline
+
+N, M, R = map(int,input().split())
+for _ in range(M):
+    u, v = map(int,input().split())
