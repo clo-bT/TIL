1fc9d501da9717e70c2982b0a55a61a1910c392d
clo-bT <tael8213@gmail.com>
2023-04-18 16:42:44 +0900

    0418엄마생일

---
+++ JS/20230418/first.js
@@ -0,0 +1,35 @@
+/*
+const a = 10
+if(true){
+    b = 20
+    console.log(a)
+
+}
+
+if(true){
+    console.log(b)
+}
+*/
+
+// const myFunction = function(x){
+//     function add(y){
+//         console.log(x + y)
+//     }
+//     return add
+// }
+
+// let func = myFunction(10)
+// func(20)
+// func(30)
+
+// func = myFunction(20)
+// func(40)
+
+function myFunction(x, y){
+    console.log(x+y)
+    console.log(arguments)
+}
+
+myFunction(10,20)
+myFunction(10,20,30,40)
+myFunction(10)
